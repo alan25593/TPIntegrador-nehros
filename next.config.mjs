1b6b27855c490848767969b3f9fb6f0e80e6ee68
Alan Lampert <60844186+alan25593@users.noreply.github.com>
2024-12-15 12:21:24 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  basePath: '', 
-  trailingSlash: false,
-  experimental: {
-    optimizeCss: true, 
-    outputFileTracing: true,
-  },
 };
 
 export default nextConfig;
