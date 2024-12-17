17b638d67393eb110773ae543098af7b91e87147
Alan Lampert <60844186+alan25593@users.noreply.github.com>
2024-12-17 11:10:35 -0300

    add export script - package.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  output: process.env.NEXT_OUTPUT || 'standalone', 
+  reactStrictMode: true,
 };
 
 export default nextConfig;
